0d842d7a4e947bd786ab01860afcf2c0420036b0
Trevor Cooper <tcooper@sdsc.edu>
2013-11-22 15:54:13 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/gamess/version.mk
+++ src/gamess/version.mk
@@ -1,3 +1,15 @@
-NAME    = gamess_$(ROLLCOMPILER)
-VERSION = 5.2012
-RELEASE = 1
+NAME               = gamess_$(ROLLCOMPILER)
+VERSION            = 5.2012
+RELEASE            = 1
+PKGROOT            = /opt/gamess
+
+SRC_SUBDIR         = gamess
+
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tgz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TGZ_PKGS           = $(SOURCE_PKG)
+
