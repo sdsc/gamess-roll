c6a9a78809bb818b4e638a6dbcf9511a9ae90349
Jerry Greenberg <jpg@sdsc.edu>
2014-01-07 21:42:20 -0800

    set release no to 0, set rpm option AutoReq:No and change SOURCE_NAME

--- src/gamess/version.mk
+++ src/gamess/version.mk
@@ -1,11 +1,12 @@
 NAME               = gamess_$(ROLLCOMPILER)
 VERSION            = 5.2012
-RELEASE            = 1
+RELEASE            = 0
 PKGROOT            = /opt/gamess
+RPM.EXTRAS         = AutoReq:No
 
 SRC_SUBDIR         = gamess
 
-SOURCE_NAME        = $(NAME)
+SOURCE_NAME        = gamess
 SOURCE_VERSION     = $(VERSION)
 SOURCE_SUFFIX      = tgz
 SOURCE_PKG         = $(SOURCE_NAME).$(SOURCE_VERSION).$(SOURCE_SUFFIX)
