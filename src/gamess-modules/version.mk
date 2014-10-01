ca6a9ec95b6e1d79e0b5ba8f9f8b63597ad8fb53
Jim Hayes <jhayes@sdsc.edu>
2014-10-01 06:22:56 -0700

    Use canonical modulefile/roll-test build files.

--- src/gamess-modules/version.mk
+++ src/gamess-modules/version.mk
@@ -1,8 +1,11 @@
-NAME        = gamess-modules
-RELEASE     = 1
-PKGROOT     = /opt/modulefiles/applications/gamess
+PACKAGE     = gamess
+CATEGORY    = applications
 
-VERSION_SRC = $(REDHAT.ROOT)/src/gamess/version.mk
+NAME        = $(PACKAGE)-modules
+RELEASE     = 2
+PKGROOT     = /opt/modulefiles/$(CATEGORY)/$(PACKAGE)
+
+VERSION_SRC = $(REDHAT.ROOT)/src/$(PACKAGE)/version.mk
 VERSION_INC = version.inc
 include $(VERSION_INC)
 
