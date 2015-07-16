0bb2ea1589a2223cfe66ec732183b27054ced9f8
Jerry Greenberg <jpg@comet-devel.sdsc.edu>
2015-07-15 17:39:01 -0700

    fix a bug where gamess may hang under mvapich2

--- src/gamess/version.mk
+++ src/gamess/version.mk
@@ -10,7 +10,7 @@ MPINAME := $(firstword $(subst /, ,$(ROLLMPI)))
 
 NAME           = sdsc-gamess
 VERSION        = 2014.12
-RELEASE        = 1
+RELEASE        = 2
 PKGROOT        = /opt/gamess
 
 SRC_SUBDIR     = gamess
