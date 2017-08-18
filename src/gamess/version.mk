788ba59c12b30d7db6d554a679efaef454ff0d01
Jerry Greenberg <jpg@sdsc.edu>
2017-08-17 17:55:15 -0700

    APS-479 gamess 2017.04

--- src/gamess/version.mk
+++ src/gamess/version.mk
@@ -9,18 +9,18 @@ endif
 MPINAME := $(firstword $(subst /, ,$(ROLLMPI)))
 
 NAME           = sdsc-gamess
-VERSION        = 2014.12
-RELEASE        = 4
+VERSION        = 2017.04
+RELEASE        = 0
 PKGROOT        = /opt/gamess
 
 SRC_SUBDIR     = gamess
 
 SOURCE_NAME    = gamess
-SOURCE_SUFFIX  = tgz
+SOURCE_SUFFIX  = tar.gz
 SOURCE_VERSION = $(VERSION)
-SOURCE_PKG     = $(SOURCE_NAME).$(SOURCE_VERSION).$(SOURCE_SUFFIX)
-SOURCE_DIR     = $(SOURCE_NAME).$(SOURCE_VERSION)
+SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR     = $(SOURCE_NAME)-$(SOURCE_VERSION)
 
-TGZ_PKGS       = $(SOURCE_PKG)
+TAR_GZ_PKGS       = $(SOURCE_PKG)
 
 RPM.EXTRAS     = AutoReq:No
