fc66a8e51065702d9df391800397deb9bcbdb02f
Jim Hayes <jhayes@sdsc.edu>
2018-03-05 15:36:18 -0800

    Add rpm make vars needed for Rocks v7. Resolves APS-703.

--- src/gamess-modules/version.mk
+++ src/gamess-modules/version.mk
@@ -2,11 +2,12 @@ PACKAGE     = gamess
 CATEGORY    = applications
 
 NAME        = sdsc-$(PACKAGE)-modules
-RELEASE     = 4
+RELEASE     = 5
 PKGROOT     = /opt/modulefiles/$(CATEGORY)/$(PACKAGE)
 
 VERSION_SRC = $(REDHAT.ROOT)/src/$(PACKAGE)/version.mk
 VERSION_INC = version.inc
 include $(VERSION_INC)
 
-RPM.EXTRAS  = AutoReq:No
+RPM.EXTRAS  = AutoReq:No\nAutoProv:No\n%define __os_install_post /usr/lib/rpm/brp-compress
+RPM.PREFIX  = $(PKGROOT)
